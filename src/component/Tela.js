0cc9ac21bc528387a5bcd13d5c898b01d45d5162
Jeremias Gabriel Sanchez <jeremiasgabrielsanchez89@gmail.com>
2023-05-22 23:29:18 -0300

    ajustes en la foto

--- src/component/Tela.js
+++ src/component/Tela.js
@@ -4,7 +4,7 @@ import { Flex, Image } from '@chakra-ui/react'
 function Tela() {
   return (
     <Flex w='100%' alignItems='center' justifyContent='center'>
-      <Image src='https://i.ytimg.com/vi/Wo9Y03EmvHY/maxresdefault.jpg' w='70%' m='1rem' border='2px' borderColor='yellow.500' borderStyle='outset'/>
+      <Image src='https://i.ytimg.com/vi/Wo9Y03EmvHY/maxresdefault.jpg' w='98%' m='1rem' border='2px' borderColor='yellow.500' borderStyle='outset' boxShadow='0px 2px 5px #000000d7'/>
     </Flex>
   )
 }
